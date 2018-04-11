664d340709ba94e538f8493971ba4a7fb4ac36bb
fernandocode <lealfernandoleal@gmail.com>
2018-04-11 08:46:00 -0300

    v.0.0.3 - published
    export version in root library.

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,4 @@ export * from "./providers/database-migration-contract";
 export * from "./providers/database-migration";
 export * from "./providers/database-helper-service";
 export * from "./defaults/mappers-table-simple";
+export * from "./model/version-model";
