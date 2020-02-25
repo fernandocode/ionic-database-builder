8bb35e9ed72c4d8eb4985505c8842e414a401d5e
fernandocode <fernandolealcode@gmail.com>
2020-02-25 16:21:59 -0300

    v.0.3.6 - added all services and classes in public api

---
+++ projects/ionic-database-builder/src/lib/model/index.ts
@@ -0,0 +1,5 @@
+
+export * from './database-config';
+export * from './database-settings-model';
+export * from './mapper-settings-model';
+export * from './version-model';
---
+++ projects/ionic-database-builder/src/lib/utils/index.ts
@@ -0,0 +1,9 @@
+
+export * from './buildable-database-manager';
+export * from './database-factory-contract';
+export * from './database-manager';
+export * from './database-migration-base';
+export * from './database-settings-factory-contract';
+export * from './dependency-injection-definition';
+export * from './platform-load-default';
+export * from './platform-load';
