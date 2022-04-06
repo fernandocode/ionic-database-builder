88bd2cf89be531b82cb8a26c0288de9db52abcda
Fernando Leal <suporte.tutor@hotmail.com>
2022-04-06 15:02:53 -0300

    versão 0.6.0-alpha.2 angular 9

--- projects/ionic-database-builder/src/lib/ionic-database-builder.module.ts
+++ projects/ionic-database-builder/src/lib/ionic-database-builder.module.ts
@@ -1,5 +1,5 @@
 import { DatabaseCreatorContract } from 'database-builder';
-import { Type, NgModule, SkipSelf, Optional } from '@angular/core';
+import { Type, NgModule, SkipSelf, Optional, ModuleWithProviders } from '@angular/core';
 import { DatabaseMigrationContract } from './services/database-migration-contract';
 import { DatabaseSettingsFactoryContract } from './utils/database-settings-factory-contract';
 import {
@@ -38,39 +38,28 @@ export class IonicDatabaseBuilderModule {
   }
 
   /** @deprecated use forRoot */
-  public static forSimple(
-    isEnableLogProvider: boolean = false,
-    isAvailableProvider: boolean = true,
-    platformLoad: Type<PlatformLoad> = PlatformLoadDefault
-  ) {
+public static forSimple(isEnableLogProvider: boolean = false, isAvailableProvider: boolean = true, platformLoad: Type<PlatformLoad> = PlatformLoadDefault): ModuleWithProviders<IonicDatabaseBuilderModule> {
     return {
-      ngModule: IonicDatabaseBuilderModule,
-      providers: [
-        { provide: IS_ENABLE_LOG, useValue: isEnableLogProvider },
-        { provide: IS_AVAILABLE_DATABASE, useValue: isAvailableProvider },
-        { provide: PLATFORM_LOAD, useClass: platformLoad },
-      ]
+        ngModule: IonicDatabaseBuilderModule,
+        providers: [
+            { provide: IS_ENABLE_LOG, useValue: isEnableLogProvider },
+            { provide: IS_AVAILABLE_DATABASE, useValue: isAvailableProvider },
+            { provide: PLATFORM_LOAD, useClass: platformLoad },
+        ]
     };
-  }
+}
 
-  public static forRoot(
-    settingsProvider: Type<DatabaseSettingsFactoryContract>,
-    databaseCreatorProvider: Type<DatabaseCreatorContract>,
-    databaseMigrationContract: Type<DatabaseMigrationContract>,
-    platformLoad: Type<PlatformLoad> = PlatformLoadDefault,
-    isEnableLogProvider: boolean = false,
-    isAvailableProvider: boolean = true,
-  ) {
+  public static forRoot(settingsProvider: Type<DatabaseSettingsFactoryContract>, databaseCreatorProvider: Type<DatabaseCreatorContract>, databaseMigrationContract: Type<DatabaseMigrationContract>, platformLoad: Type<PlatformLoad> = PlatformLoadDefault, isEnableLogProvider: boolean = false, isAvailableProvider: boolean = true): ModuleWithProviders<IonicDatabaseBuilderModule> {
     return {
-      ngModule: IonicDatabaseBuilderModule,
-      providers: [
-        { provide: DatabaseSettingsFactoryContract, useClass: settingsProvider },
-        { provide: DATABASE_CREATOR, useClass: databaseCreatorProvider },
-        { provide: DATABASE_MIGRATION, useClass: databaseMigrationContract },
-        { provide: IS_ENABLE_LOG, useValue: isEnableLogProvider },
-        { provide: PLATFORM_LOAD, useClass: platformLoad },
-        { provide: IS_AVAILABLE_DATABASE, useValue: isAvailableProvider },
-      ]
+        ngModule: IonicDatabaseBuilderModule,
+        providers: [
+            { provide: DatabaseSettingsFactoryContract, useClass: settingsProvider },
+            { provide: DATABASE_CREATOR, useClass: databaseCreatorProvider },
+            { provide: DATABASE_MIGRATION, useClass: databaseMigrationContract },
+            { provide: IS_ENABLE_LOG, useValue: isEnableLogProvider },
+            { provide: PLATFORM_LOAD, useClass: platformLoad },
+            { provide: IS_AVAILABLE_DATABASE, useValue: isAvailableProvider },
+        ]
     };
-  }
+}
 }
--- projects/ionic-database-builder/src/lib/utils/platform-load-default.ts
+++ projects/ionic-database-builder/src/lib/utils/platform-load-default.ts
@@ -1,5 +1,7 @@
 import { PlatformLoad } from './platform-load';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class PlatformLoadDefault extends PlatformLoad {
     public ready(): Promise<void> {
         return Promise.resolve();
